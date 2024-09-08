fdbf5c1dcfd8fe1978f831ad88fd4c6ec258b39e
Vineet Mishra <02vineetmishra@gmail.com>
2024-09-08 17:57:03 +0530

    cors updates

--- server.js
+++ server.js
@@ -16,8 +16,15 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
+app.use(
+  cors({
+    origin: '*', // Allows all origins, adjust as needed
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
+
 app.use(morgan('dev'));
 
 // Serve static files from the 'public/uploads' folder
